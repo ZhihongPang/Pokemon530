53e489a9104e9f0d8d73438d214618f61d9143af
Serohyoon <78445463+vg16855@users.noreply.github.com>
2022-04-07 22:36:17 -0400

    Edited models to actually implement a database
    fixes #2

--- Pokemon530/PseudomonGo/models.py
+++ Pokemon530/PseudomonGo/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+class Player(models.Model):
+    username = models.CharField(max_length=50)
+    level = models.IntegerField()
+    num_animals = models.IntegerField()
+
+
+class Animal(models.Model):
+    owner_id = models.ForeignKey(Player, on_delete=models.CASCADE)
+    animal_name = models.CharField(max_length=50)
+    animal_type = models.CharField(max_length=50)
+    animal_species = models.CharField(max_length=50)
+    photograph_path = models.CharField(max_length=100)
+    health = models.FloatField()
+    attack = models.FloatField()
+    defense = models.FloatField()
+    speed = models.FloatField()
+
+
+class Robot(models.Model):
+    type = models.CharField(max_length=25)
+    health = models.FloatField()
+    attack = models.FloatField()
+    defense = models.FloatField()
+    speed = models.FloatField()
