dd37e9935f6c3b17fd9f15890411322028cf03e0
Sage Garcia <uh56499@umbc.edu>
2022-04-21 18:02:29 -0400

    added tests for APIs

--- Pokemon530/PseudomonGo/tests/test_user_api.py
+++ Pokemon530/PseudomonGo/tests/test_user_api.py
@@ -3,12 +3,12 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 from rest_framework.test import force_authenticate
 from django.contrib.auth.models import User
-from rest_framework.test import APIRequestFactory
 
 class UserAPITests(APITestCase):
 
-    def setup(self):
-        self.user = User.objects.create(email="test@test.com",username='testuser', password='12345')
+    def setUp(self):
+        user = User.objects.create_user(email="test@test.com",username='testuser',password='12345')
+        user.save()
 
     def test_registration(self):
         data = {
@@ -18,4 +18,21 @@ class UserAPITests(APITestCase):
         }
         response = self.client.post('/api/register/', data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_login(self):
+        data = {
+            'email': 'test@test.com',
+            'password': '12345'
+        }
+        response = self.client.post('/api/login/', data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_auth_user(self):
+        # todo
+        pass
+
+    def test_logout(self):
+        response = self.client.post('/api/logout/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
         
