b7e691ae09d6be1a573fe2e14ab2b8121729932d
SAGE-GARCIA-UMBC <uh56499@umbc.edu>
2022-05-17 18:49:43 -0400

    added allowed host

--- Pokemon530/Pokemon530/settings.py
+++ Pokemon530/Pokemon530/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pseudomongo-530-447.herokuapp.com']
 
 
 # Application definition
