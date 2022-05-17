aea78d0d9f64acb6c5e16008e94904ac19d18b9c
SAGE-GARCIA-UMBC <uh56499@umbc.edu>
2022-05-17 18:36:50 -0400

    changes to settings

--- Pokemon530/Pokemon530/settings.py
+++ Pokemon530/Pokemon530/settings.py
@@ -136,14 +136,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
